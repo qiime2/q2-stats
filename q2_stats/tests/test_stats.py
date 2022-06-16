bb0ab11f5090203b0fe05f05a68f1c398ae261e4
Chloe Herman <60228108+cherman2@users.noreply.github.com>
2022-06-16 13:28:47 -0700

    porting _validator.py

--- q2_stats/tests/test_stats.py
+++ q2_stats/tests/test_stats.py
@@ -10,10 +10,13 @@ import pandas as pd
 import numpy as np
 
 from qiime2.plugin.testing import TestPluginBase
+from qiime2.plugin import ValidationError
 
 from q2_stats._stats import wilcoxon_srt, mann_whitney_u
 from q2_stats._examples import (faithpd_timedist_factory,
                                 faithpd_refdist_factory)
+from q2_stats._validator import (validate_all_dist_columns_present,
+                                 validate_unique_subjects_within_group)
 
 
 class TestBase(TestPluginBase):
@@ -227,3 +230,26 @@ class TestStats(TestBase):
 
     def test_examples(self):
         self.execute_examples()
+
+
+class TestValidators(TestBase):
+    def test_validators_missing_columns_in_dist(self):
+        with self.assertRaisesRegex(ValidationError, '"group" not found'
+                                    ' in distribution.'):
+            df = pd.DataFrame({
+                'id': ['S340445', 'S892825', 'S460691'],
+                'measure': [7.662921088, 8.431734297, 8.513263823]
+            })
+            validate_all_dist_columns_present(df, level=min)
+
+    def test_validators_unique_subjects_not_duplicated_per_group(self):
+        with self.assertRaisesRegex(ValidationError, 'Unique subject found'
+                                    ' more than once within an individual'
+                                    ' group.*0.*P26'):
+            df = pd.DataFrame({
+                'id': ['S116625', 'S813956'],
+                'measure': [7.662921088, 8.431734297],
+                'group': [0, 0],
+                'subject': ['P26', 'P26']
+            })
+            validate_unique_subjects_within_group(df, level=min)
